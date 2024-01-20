aa99e43a4ed2421e3561f39a7567d841a68bebc4
RudraPatel432 <rudrapatel0432@gmail.com>
2024-01-19 19:39:24 -0500

    x

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll('.sections section');
     const navLinks = document.querySelectorAll('.navbar a');
 
     navLinks.forEach(function (link) {
@@ -11,18 +10,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     function scrollToSectionHeading(sectionId) {
-        const section = document.querySelector('#' + sectionId);
-        if (section) {
-            const heading = section.querySelector('h1, h2, h3, h4, h5, h6');
-            if (heading) {
-                const rect = heading.getBoundingClientRect();
-                const scrollPosition = window.scrollY + (rect.top + rect.height / 2) - (window.innerHeight / 2);
-                window.scrollTo({
-                    top: scrollPosition,
-                    behavior: 'smooth'
-                });
+        // Check if sectionId is not empty
+        if (sectionId) {
+            const section = document.getElementById(sectionId);
+
+            // Check if the section exists
+            if (section) {
+                const heading = section.querySelector('h1, h2, h3, h4, h5, h6');
+
+                // Check if the heading exists within the section
+                if (heading) {
+                    const rect = heading.getBoundingClientRect();
+                    const scrollPosition = window.scrollY + (rect.top + rect.height / 2) - (window.innerHeight / 2);
+
+                    window.scrollTo({
+                        top: scrollPosition,
+                        behavior: 'smooth'
+                    });
+                }
             }
         }
+        
     }
-
 });
