7098e747298db2ae0cebb56801c5c167a914912f
RudraPatel432 <rudrapatel0432@gmail.com>
2024-03-01 07:23:00 -0500

    Added Links to courses

--- src/js/edu.js
+++ src/js/edu.js
@@ -1,53 +1,58 @@
-// education.js
-
 function addCourseDetails(term, courses) {
-    var container = document.createElement('div');
-    container.classList.add('timeline-node-container');
+	var container = document.createElement("div");
+	container.classList.add("timeline-node-container");
 
-    var details = document.createElement('div');
-    details.classList.add('course-details');
+	var details = document.createElement("div");
+	details.classList.add("course-details");
 
-    var termElement = document.createElement('p');
-    termElement.textContent = term;
-    details.appendChild(termElement);
+	var termElement = document.createElement("p");
+	termElement.textContent = term;
+	details.appendChild(termElement);
 
-    var coursesList = document.createElement('ul');
-    courses.forEach(function(course) {
-        var courseItem = document.createElement('li');
-        courseItem.textContent = course;
-        coursesList.appendChild(courseItem);
-    });
-    details.appendChild(coursesList);
+	var coursesList = document.createElement("ul");
+	courses.forEach(function (course) {
+		var courseItem = document.createElement("li");
+		var courseLink = document.createElement("a");
+		courseLink.textContent = course.name;
+		courseLink.href = course.link;
+		courseLink.target = "_blank"; // Open link in new tab
+		courseItem.appendChild(courseLink);
+		coursesList.appendChild(courseItem);
+	});
+	details.appendChild(coursesList);
 
-    container.appendChild(details);
-    console.log(container.appendChild(details))
-    return container;
+	container.appendChild(details);
+	return container;
 }
 
 function createTimelineLine() {
-    var timelineLine = document.createElement('div');
-    timelineLine.classList.add('timeline-line');
-    return timelineLine;
+	var timelineLine = document.createElement("div");
+	timelineLine.classList.add("timeline-line");
+	return timelineLine;
 }
 
 function populateEducationTimeline() {
-    var timeline = document.getElementById('educationTimeline');
-    
-    // Create and append timeline line
-    var timelineLine = createTimelineLine();
-    timeline.appendChild(timelineLine);
+	var timeline = document.getElementById("educationTimeline");
 
-    // Fetch JSON data
-    fetch('/rudra-patel-v1/src/js/edu.json')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(function(termAndCourses) {
-                var node = addCourseDetails(termAndCourses.term, termAndCourses.courses);
-                timeline.appendChild(node);
-            });
-        })
-        .catch(error => console.error('Error fetching JSON data:', error));
+	// Create and append timeline line
+	var timelineLine = createTimelineLine();
+	timeline.appendChild(timelineLine);
+
+	// Fetch JSON data
+	fetch("/rudra-patel-v1/src/js/edu.json")
+		.then((response) => response.json())
+		.then((data) => {
+			data.forEach(function (termAndCourses) {
+				var node = addCourseDetails(
+					termAndCourses.term,
+					termAndCourses.courses
+				);
+				timeline.appendChild(node);
+			});
+		})
+		.catch((error) => console.error("Error fetching JSON data:", error));
 }
+
 // Sliding this here for skills
 function toggleActive(element) {
 	element.classList.toggle("active");
@@ -62,5 +67,6 @@ function toggleActive(element) {
 		element.style.color = "";
 	}
 }
+
 // Call the function when the DOM is ready
-document.addEventListener('DOMContentLoaded', populateEducationTimeline);
+document.addEventListener("DOMContentLoaded", populateEducationTimeline);
