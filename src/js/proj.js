506ec6549116858c5d4f5f56ff90fdb69505b401
RudraPatel432 <rudrapatel0432@gmail.com>
2024-03-05 19:50:43 -0500

    Old

--- src/js/proj.js
+++ src/js/proj.js
@@ -1,28 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Fetch project data from proj.json
-    fetch('/rudra-patel-v1/src/js/proj.json')
-        .then(response => response.json())
-        .then(data => {
-            // Iterate through each project and add dynamically to the HTML
-            data.forEach(project => {
-                addProject(project);
-            });
+document.addEventListener("DOMContentLoaded", function () {
+	// Fetch project data from proj.json
+	fetch("/rudra-patel-v1/src/js/proj.json")
+		.then((response) => response.json())
+		.then((data) => {
+			// Iterate through each project and add dynamically to the HTML
+			data.forEach((project) => {
+				addProject(project);
+			});
 
-            // Add click and touch event listener to each project
-            const projectElements = document.querySelectorAll('.project');
-            projectElements.forEach((projectElement, index) => {
-                projectElement.addEventListener('click', () => {
-                    handleProjectClick(projectElement, data[index]);
-                });
+			// Add click and touch event listener to each project
+			const projectElements = document.querySelectorAll(".project");
+			projectElements.forEach((projectElement, index) => {
+				projectElement.addEventListener("click", () => {
+					handleProjectClick(projectElement, data[index]);
+				});
 
-                // Add touch event listener
-                projectElement.addEventListener('touchstart', (event) => {
-                    event.preventDefault();
-                    handleProjectClick(projectElement, data[index]);
-                });
-            });
-        })
-        .catch(error => console.error('Error fetching project data:', error));
+				// Add touch event listener
+				projectElement.addEventListener("touchstart", (event) => {
+					event.preventDefault();
+					handleProjectClick(projectElement, data[index]);
+				});
+			});
+		})
+		.catch((error) => console.error("Error fetching project data:", error));
 });
 
 function addProject(project) {
@@ -36,18 +36,7 @@ function addProject(project) {
 
 	const description = document.createElement("p");
 	description.className = "project-meta";
-	const truncatedDescription =
-		project.description.substring(0, 150) +
-		(project.description.length > 150 ? "..." : "");
-	description.textContent = truncatedDescription;
-
-	const fullDescriptionButton = document.createElement("button");
-    fullDescriptionButton.className = "fullDescriptionButton";
-	fullDescriptionButton.textContent = "...";
-	fullDescriptionButton.addEventListener("click", () => {
-		description.textContent = project.description;
-		fullDescriptionButton.style.display = "none";
-	});
+	description.textContent = project.description;
 
 	const tags = document.createElement("div");
 	tags.className = "tags";
@@ -64,9 +53,6 @@ function addProject(project) {
 	// Append elements to the project div
 	projectDiv.appendChild(heading);
 	projectDiv.appendChild(description);
-	if (project.description.length > 150) {
-		projectDiv.appendChild(fullDescriptionButton);
-	}
 	projectDiv.appendChild(tags);
 	projectDiv.appendChild(completionDate);
 
@@ -75,10 +61,9 @@ function addProject(project) {
 	projectsContainer.appendChild(projectDiv);
 }
 
-
 function handleProjectClick(project) {
-    // Define the behavior when a project is clicked
-    // For example, you can open a new page with more details
-    console.log('Project clicked:', project);
-    // Add your code to handle the click event, e.g., open a new page or display more details
+	// Define the behavior when a project is clicked
+	// For example, you can open a new page with more details
+	console.log("Project clicked:", project);
+	// Add your code to handle the click event, e.g., open a new page or display more details
 }
