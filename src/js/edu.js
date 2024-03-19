65419323ae580abc9f485c801a361a7bfbe6da71
rrp432 <rrp3827@gmail.com>
2024-03-19 11:24:25 -0400

    updated cllg logo

--- src/js/edu.js
+++ src/js/edu.js
@@ -1,6 +1,10 @@
+// Fetch the JSON data
 fetch('/rudra-patel-v1/src/js/edu.json')
 	.then(response => response.json())
-	.then(data => populateEducationTimeline(data))
+	.then(data => {
+		populateEducationTimeline(data);
+		appendCollegeLogos();
+	})
 	.catch(error => console.error('Error fetching JSON data:', error));
 
 function populateEducationTimeline(data) {
@@ -9,19 +13,6 @@ function populateEducationTimeline(data) {
 	data.forEach(college => {
 		const universityContainer = document.createElement('div');
 		universityContainer.classList.add('timeline-item');
-
-		const universityName = document.createElement('h2');
-		universityName.classList.add('university-name');
-		universityName.textContent = college.college;
-
-		// Add event listener to college name
-		universityName.addEventListener('click', () => {
-			// Toggle visibility of course list
-			const courseList = universityContainer.querySelector('.course-list');
-			courseList.classList.toggle('show-course-list');
-		});
-
-		universityContainer.appendChild(universityName);
 
 		const courseList = document.createElement('div');
 		courseList.classList.add('course-list');
@@ -37,4 +28,33 @@ function populateEducationTimeline(data) {
 
 		timelineContainer.appendChild(universityContainer);
 	});
+}
+
+function appendCollegeLogos() {
+	const hcccLogo = createLogoElement('/rudra-patel-v1/src/img/hcccLogo.png', 'HCCC Logo', 'logo', 'college-logo', 'hccc-logo');
+	const oitLogo = createLogoElement('/rudra-patel-v1/src/img/oitLogo.png', 'OIT Logo', 'logo', 'college-logo', 'oit-logo');
+	const rutgersLogo = createLogoElement('/rudra-patel-v1/src/img/rutgersLogo.png', 'Rutgers Logo', 'logo', 'college-logo', 'rutgers-logo');
+
+	const timelineItems = document.querySelectorAll('.timeline-item');
+
+	timelineItems.forEach((item, index) => {
+		const logo = index === 0 ? oitLogo : index === 1 ? hcccLogo : rutgersLogo;
+
+		// Add event listener to logo to toggle course list visibility
+		logo.addEventListener('click', () => {
+			const courseList = item.querySelector('.course-list');
+			courseList.classList.toggle('show-course-list');
+		});
+
+		item.insertBefore(logo, item.firstChild);
+	});
+}
+
+function createLogoElement(src, alt, ...classNames) {
+	const logo = document.createElement('img');
+	logo.src = src;
+	logo.alt = alt;
+	logo.classList.add(...classNames);
+
+	return logo;
 }
